48faaf405d42eb5ef2fc2100dd734404d461d546
tsynaev <1083414+tsynaev@users.noreply.github.com>
2026-01-05 18:28:25 +0100

    Add MudBlazor multileg position builder

--- BlazorOptions/Program.cs
+++ BlazorOptions/Program.cs
@@ -1,6 +1,7 @@
 using BlazorOptions;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -10,5 +11,6 @@ builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.
 
 // register the options helper/service used by the chart page
 builder.Services.AddSingleton<OptionsService>();
+builder.Services.AddMudServices();
 
 await builder.Build().RunAsync();
