1e0b73049a8c73f594dae2eaaac3fb8eeacb56fd
Aleksey Tsynaev <alt@inetlab.com>
2026-01-11 13:02:20 +0100

    Add Server app

---
+++ BlazorOptions.Server/Program.cs
@@ -0,0 +1,22 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.UseWebAssemblyDebugging();
+}
+
+app.UseBlazorFrameworkFiles();   // serve _framework from Client build
+app.UseStaticFiles();
+
+app.MapFallbackToFile("index.html");
+
+app.Run();
+
